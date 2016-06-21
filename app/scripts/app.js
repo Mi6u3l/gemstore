27e18a800d5c52943b7f54ba49889ab6afcf33d8
Mi6u3l <miguel.bgomes@gmail.com>
2016-06-21 10:56:35 +0300

    initial project setup, create modules and controller

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name gemstoreApp
+ * @description
+ * # gemstoreApp
+ *
+ * Main module of the application.
+ */
+ angular.module('gemStore', [
+     'gemStore.controllers'
+ ])
