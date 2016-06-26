3e2d109e0212ba7642cbb4e3c334aa4a69097388
Mi6u3l <miguel.bgomes@gmail.com>
2016-06-26 12:53:48 +0100

    add views, replaced controllers with directives

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -79,6 +79,59 @@ var gems = [{
 
 angular.module('gemStore.controllers', []).
 
+    directive('productDescription', function() {
+       return {
+       restrict: 'E',
+       templateUrl: '/views/product-description.html'
+     };
+    }).
+
+    directive("productTabs", function() {
+        return {
+            restrict: 'E',
+            templateUrl: "/views/product-tabs.html",
+            controller:function(){
+                this.tab = 1;
+                this.isSet = function(checkTab) {
+                    return this.tab === checkTab;
+                };
+                this.setTab = function(setTab) {
+                    this.tab = setTab;
+                };
+            },
+            controllerAs: "tab"
+        };
+    }).
+
+    directive('productSpecs', function() {
+       return {
+       restrict: 'A',
+       templateUrl: '/views/product-specs.html'
+     };
+    }).
+
+    directive("productReviews", function() {
+        return {
+        restrict: 'E',
+        templateUrl: "/views/product-reviews.html"
+      };
+    }).
+
+    directive("productGallery", function() {
+    return {
+      restrict: 'E',
+      templateUrl: "/views/product-gallery.html",
+      controller: function(){
+        this.current = 0;
+
+        this.setCurrent = function(imageNumber){
+        this.current = imageNumber || 0;
+        };
+      },
+      controllerAs:'gallery'
+     };
+    }).
+
     controller('galleryController', function(){
         this.current = 0;
         this.setCurrent = function(value)
@@ -100,17 +153,4 @@ angular.module('gemStore.controllers', []).
             this.review.createdOn = date.now();
             this.review = {};
         };
-    }).
-    controller('tabController', function(){
-        this.tab = 1;
-        this.setTab = function(value){
-            this.tab = value;
-        };
-        this.isSet = function(value)
-        {
-            if (this.tab === value)
-                return true;
-            else
-                return false;
-        };
-});
+    });
